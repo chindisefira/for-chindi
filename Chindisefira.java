dd3b62b828b66f4e80e1b2d383330fa86c9d419f
chindisefira <39687656+chindisefira@users.noreply.github.com>
2018-05-30 21:44:25 +0800

    Add files via upload

---
+++ Chindisefira.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chindisefira;
+
+/**
+ *
+ * @author Pratama
+ */
+public class Chindisefira {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for(int i=1; i<11 ;i++){
+                System.out.println(i + "  * 8 = " + (i*8));
+        }
+    }
+}
